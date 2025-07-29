1975a6e1e4fb8a7c1aea62944ad4144ae5ab382f
Sanjjiiev S <sanjjiiev005@gmail.com.com>
2025-07-29 18:33:15 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 import StudentGradingSystem from "./components/Grade";
 
